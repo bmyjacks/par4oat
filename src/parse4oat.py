0cdab3c455b27ae6b21870778a3bb9cb36856ad1
Mingyi BAO <bmyjacks@outlook.com>
2025-04-07 09:29:05 +0800

    Add parse4oat

---
+++ src/parse4oat.py
@@ -0,0 +1,9 @@
+class Parse4Oat:
+    def __init__(self, parse_table):
+        self.parse_table = parse_table
+        self.stack = []
+        self.input = []
+        self.output = []
+
+    def parse(self, input_string):
+        pass
